b85a4b1f8740dfa2bbd89989216bdffa73a95a0f
Abhinav Sundaram <esabhinav@gmail.com>
2020-07-20 11:37:28 +0530

    Update updateCount.js

--- js/updateCount.js
+++ js/updateCount.js
@@ -1,11 +1,3 @@
-const countEl = document.getElementById('count');
-
-updateVisitCount();
-
-function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1')
-	.then(res => res.json())
-	.then(res => {
-		countEl.innerHTML = res.value;
-	})
+function cb(response) {
+    document.getElementById('visits').innerText = response.value;
 }
